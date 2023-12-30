0178c6e4f9fc2acea985f0bdec7bd8afe0df18bb
gyubin <gbcompany27@gmail.com>
2023-12-30 16:52:57 +0900

    default setting for uploading image

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: "uploads" })
+
+const app = express();
+const PORT = 3000;
+
+app.post('/upload', upload.single("image"), (req,res) => {
+    console.log(req.file);
+    res.json(req.file);
+})
+
+app.listen(PORT, () => console.log("Express server liseting on PORT " + PORT));
