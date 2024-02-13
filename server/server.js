47ddf92908f060bd18a1fe0094ad017dc9f1b476
Gyubin Sim <gyubinsim@Gyubinui-MacBookAir.local>
2024-02-13 12:36:33 +0900

    create server folder and modify file tree

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const multer = require('multer');
+const { v4: uuid } = require('uuid');
+const mime = require('mime-types');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads"),
+    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+})
+const upload = multer({
+    storage, 
+    fileFilter: (req, file, cb) => {
+        if (['image/jpeg','image/jpg','image/png'].includes(file.mimetype)) cb(null, true)
+        else cb(new Error('Invalid file type'), false);
+    },
+    limits: {
+        fileSize: 5* 1024 * 1024
+    }
+});
+
+const app = express();
+const PORT = 3000;
+
+app.use("/uploads",express.static("uploads"));
+
+app.post('/upload', upload.single("imageTest"), (req,res) => {
+    console.log(req.file);
+    res.json(req.file);
+})
+
+app.listen(PORT, () => console.log("Express server liseting on PORT " + PORT));
