be32fb1682d6569d9d0bf46299d22f5e308a4475
Gyubin Sim <gyubinsim@Gyubinui-MacBookAir.local>
2024-02-13 12:33:52 +0900

    use uuid as image file name and add filter when saving an image file

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: "uploads" })
+const { v4: uuid } = require('uuid');
+const mime = require('mime-types');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads"),
+    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+})
+const upload = multer({
+    storage, 
+    fileFilter: (req, file, cb) => {
+        if (['image/jpeg','image/jpg','image/png'].includes(file.mimetype)) cb(null, true)
+        else cb(new Error('Invalid file type'), false);
+    },
+    limits: {
+        fileSize: 5* 1024 * 1024
+    }
+});
 
 const app = express();
 const PORT = 3000;
 
-app.post('/upload', upload.single("image"), (req,res) => {
+app.use("/uploads",express.static("uploads"));
+
+app.post('/upload', upload.single("imageTest"), (req,res) => {
     console.log(req.file);
     res.json(req.file);
 })
