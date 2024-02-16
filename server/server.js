9e9d210160fecde5fd6e2d6e9fab1821852ee060
Gyubin <gbcompany27@gmail.com>
2024-02-16 22:17:26 +0900

    fix the bug

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const { imageRouter } = require("./routes/imageRouter");
+const { userRouter } = require("./routes/userRouter");
 
 const app = express();
 const { MONGODB_URI, PORT } = process.env;
@@ -14,7 +15,9 @@ mongoose.connect(
 .then(() => {
     console.log("MongoDB connected") 
     app.use("/uploads",express.static("uploads"));
-    app.use("/images", imageRouter)
+    app.use(express.json());
+    app.use("/images", imageRouter);
+    app.use("/users",userRouter);
     app.listen(PORT, () => console.log("Express server liseting on PORT " + PORT));  
 })
 .catch((err)=>console.log(err));
